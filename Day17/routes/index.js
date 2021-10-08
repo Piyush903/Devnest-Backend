0855394eca2cc1cb27dc49dc294f319be4dfcc20
Piyush Nijhawan <piyushnijhawan78@gmail.com>
2021-10-08 20:04:23 +0530

    day17

---
+++ Day17/routes/index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+var registerInitialChecks = require("../middlewares/registerChecks");
+var register = require("../controllers/register");
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const sess = req.session;
+  sess.username = "piyush";
+  res.render("index", { title: "Express" });
+});
+router.get("/test", function (req, res, next) {
+  console.log("Redis Value", req.session.username);
+  const sess = req.session;
+  res.render("index", { title: "Express" });
+});
+
+/**
+ * @requires {email , password , cofirm password} -req.body
+ * @description
+ * Security , Performances , Edge Cases
+ * Lv -1
+ * email validate -string
+ * password validation
+ * password == confirm password
+ * Lv -2
+ * JS/SQL injection
+ * Lv-3
+ * Check if email already exists
+ * password hashing - security
+ * email lowercase
+ * save
+ */
+
+router.post("/register", (registerInitialChecks, register));
+module.exports = router;
