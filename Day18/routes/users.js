6b519f9829eb10b749c29f97dff33162c56c1391
Piyush Nijhawan <piyushnijhawan78@gmail.com>
2021-10-15 23:25:28 +0530

    day20 added

---
+++ Day18/routes/users.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "devs",
+  password: "ppp",
+  port: 5432,
+});
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  pool.query(
+    `SELECT * FROM  "Users" where id=$1
+    `,
+    [req.query.id],
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.status(200).json(result.rows);
+    }
+  );
+});
+
+module.exports = router;
---
+++ Day20/models/mongo.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  fullname: { type: String, default: "Number" },
+  email: { type: String, default: "Number" },
+  password: {
+    type: String,
+    default: "Number",
+  },
+});
+
+module.exports = User = mongoose.model("User", UserSchema);
